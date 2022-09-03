8c44b358d207c0c83f90305895b15dfd5c7530c0
Bipul-hossen <mdbipulhossain252@gmail.com>
2022-09-03 21:22:48 +0600

    card seloved

--- js/index.js
+++ js/index.js
@@ -25,23 +25,37 @@ displayApiForNav()
 function addToPlayer(element) {
     const newsId = element.parentNode.children[0].id;
     loadData(newsId)
+    const cetagoryName = element.parentNode.children[0].innerText//select cetagory name
+    const selectCetagoryFeild = document.getElementById('items-category')//select cetagory feild
+    const selectCetagoryString = selectCetagoryFeild.innerText;
+    const selectCetagory = parseFloat(selectCetagoryString);
+    selectCetagoryFeild.innerText = cetagoryName
+
+
 }
 
-const loadData = async (id = 08) => {
+const loadData = async (id='01') => {
     //console.log(id)
     id.textContent = ""
     fetch(`https://openapi.programming-hero.com/api/news/category/${id} `)
         .then(response => response.json())
         .then(data => displayLoadData(data.data))
-    console.log(data.data)
+    //  console.log(data.data)
 }
 loadData()
 
 const displayLoadData = async (data) => {
+    //items no. assign
+    const selectItemsFeild = document.getElementById('items-number')
+    const selectItemsString = selectItemsFeild.innerText;
+    const selectItems = parseFloat(selectItemsFeild)
+    selectItemsFeild.innerText = data.length
+
+
     const cardContainer = document.getElementById("card-items");
     cardContainer.textContent = ""
     data.forEach(data => {
-       
+
         //console.log(data)
         const { details, image_url, title } = data;
 
@@ -54,8 +68,9 @@ const displayLoadData = async (data) => {
                     <p>${details.length > 20 ? details.slice(0, 70) + ".." : details}</p>
                     <p>price: </p>
                     <div class="card-actions justify-end">
-                    <label for="my-modal-3" 
-                    onclick="showModal('')"  class="btn btn-primary modal-button">Show Detail</label>
+                    
+                    <label for="my-modal-3">
+                    <a id="" class="btn modal-button text-2xl mx-2" onclick="showModal('${details}','${image_url}','${title}')"> <i class="fa-solid fa-arrow-right"></i></a>
                     </div>
                 </div>
             </div> 
@@ -65,6 +80,15 @@ const displayLoadData = async (data) => {
 
 }
 
-
+const showModal = (details, image_url, title) => {
+    //console.log(details, image_url, title)
+    const modalBody = document.getElementById("modal-body");
+    modalBody.textContent = "";
+    modalBody.innerHTML = `
+    <p class="py-4 card-title text-primary">${title}</p>
+    <img src="${image_url}"/>
+    <p class="py-4">${details}</p>
+    `
+}
 
 
